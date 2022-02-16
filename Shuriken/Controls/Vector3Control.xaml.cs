241ab1aad07ebb8faa14757bd17261d7243fdea4
Crash5b <crash5band@gmail.com>
2022-02-16 13:36:10 +0200

    Update controls as per last commit

---
+++ Shuriken/Controls/Vector3Control.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Shuriken.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shuriken.Controls
+{
+    /// <summary>
+    /// Interaction logic for Vector3Control.xaml
+    /// </summary>
+    public partial class Vector3Control : UserControl
+    {
+        public Vector3Control()
+        {
+            InitializeComponent();
+            LayoutRoot.DataContext = this;
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the bound Vector2 object
+        /// </summary>
+        public Vector3 Value
+        {
+            get => (Vector3)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value", typeof(Vector3), typeof(Vector3Control), new PropertyMetadata(new Vector3()));
+
+    }
+}
