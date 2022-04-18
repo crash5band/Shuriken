f96539c5daa1c37123f435c7d71457f475e885ba
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-04-18 22:47:48 +0100

    EndChunk doesn't have size

--- XNCPLib/XNCP/EndChunk.cs
+++ XNCPLib/XNCP/EndChunk.cs
@@ -9,27 +9,42 @@ namespace XNCPLib.XNCP
 {
     public class EndChunk
     {
-        public ChunkHeader Header { get; set; }
+        public uint Signature { get; set; }
         public uint[] Padding { get; set; }
 
         public EndChunk()
         {
-            Header = new ChunkHeader();
-            Padding = new uint[] { 0, 0 };
+            Padding = new uint[] { 0, 0, 0 };
         }
 
         public void Read(BinaryObjectReader reader)
         {
-            Header.Read(reader);
+            // Header is always little endian
+            Endianness endianPrev = reader.Endianness;
+            reader.Endianness = Endianness.Little;
+            {
+                Signature = reader.ReadUInt32();
+            }
+            reader.Endianness = endianPrev;
+
             Padding[0] = reader.ReadUInt32();
             Padding[1] = reader.ReadUInt32();
+            Padding[2] = reader.ReadUInt32();
         }
 
         public void Write(BinaryObjectWriter writer)
         {
-            Header.Write(writer);
+            // Header is always little endian
+            Endianness endianPrev = writer.Endianness;
+            writer.Endianness = Endianness.Little;
+            {
+                writer.WriteUInt32(Signature);
+            }
+            writer.Endianness = endianPrev;
+
             writer.WriteUInt32(Padding[0]);
             writer.WriteUInt32(Padding[1]);
+            writer.WriteUInt32(Padding[2]);
         }
     }
 }
