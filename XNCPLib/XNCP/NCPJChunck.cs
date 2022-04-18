0159ae55506ee35cad8fee387f01ab7f85951532
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-04-18 23:49:12 +0100

    Write NCPJChunk dynamically

--- XNCPLib/XNCP/NCPJChunck.cs
+++ XNCPLib/XNCP/NCPJChunck.cs
@@ -9,20 +9,16 @@ namespace XNCPLib.XNCP
     public class NCPJChunck
     {
         public bool IsUsed { get; set; }
-        public ChunkHeader Header { get; set; }
+        public uint Signature { get; set; }
         public uint Field08 { get; set; }
         public uint Field0C { get; set; }
-        public uint RootNodeOffset { get; set; }
         public string ProjectName { get; set; }
-        private uint ProjectNameOffset { get; set; }
         public uint DXLSignature { get; set; }
-        public uint FontListOffset { get; set; }
         public CSDNode Root { get; set; }
         public FontList Fonts { get; set; }
 
         public NCPJChunck()
         {
-            Header = new ChunkHeader();
             Root = new CSDNode();
             Fonts = new FontList();
         }
@@ -32,24 +28,35 @@ namespace XNCPLib.XNCP
             IsUsed = true;
 
             reader.PushOffsetOrigin();
-            Header = new ChunkHeader();
-            Header.Read(reader);
+            Endianness endianPrev = reader.Endianness;
+
+            // Header is always little endian
+            uint size;
+            reader.Endianness = Endianness.Little;
+            {
+                Signature = reader.ReadUInt32();
+                size = reader.ReadUInt32();
+            }
+            reader.Endianness = endianPrev;
 
             Field08 = reader.ReadUInt32();
             Field0C = reader.ReadUInt32();
-            RootNodeOffset = reader.ReadUInt32();
 
-            ProjectNameOffset = reader.ReadUInt32();
-            ProjectName = reader.ReadStringOffset(ProjectNameOffset);
+            uint rootNodeOffset = reader.ReadUInt32();
+
+            uint projectNameOffset = reader.ReadUInt32();
+            ProjectName = reader.ReadStringOffset(projectNameOffset);
 
             DXLSignature = reader.ReadUInt32();
-            FontListOffset = reader.ReadUInt32();
+            uint fontListOffset = reader.ReadUInt32();
 
-            reader.Seek(reader.GetOffsetOrigin() + RootNodeOffset, SeekOrigin.Begin);
+            reader.Seek(reader.GetOffsetOrigin() + rootNodeOffset, SeekOrigin.Begin);
             Root.Read(reader);
 
-            reader.Seek(reader.GetOffsetOrigin() + FontListOffset, SeekOrigin.Begin);
+            reader.Seek(reader.GetOffsetOrigin() + fontListOffset, SeekOrigin.Begin);
             Fonts.Read(reader);
+
+            // TODO: can we verify the the position after the last root/font matches the size?
 
             reader.PopOffsetOrigin();
         }
@@ -57,23 +64,55 @@ namespace XNCPLib.XNCP
         public void Write(BinaryObjectWriter writer)
         {
             writer.PushOffsetOrigin();
-            Header.Write(writer);
+            Endianness endianPrev = writer.Endianness;
+
+            // Header is always little endian
+            writer.Endianness = Endianness.Little;
+            {
+                writer.WriteUInt32(Signature);
+
+                // Skipped: size
+                writer.Skip(4);
+            }
+            writer.Endianness = endianPrev;
+            uint dataStart = (uint)writer.Position;
 
             writer.WriteUInt32(Field08);
             writer.WriteUInt32(Field0C);
-            writer.WriteUInt32(RootNodeOffset);
 
-            writer.WriteUInt32(ProjectNameOffset);
-            writer.WriteStringOffset(ProjectNameOffset, ProjectName);
+            // TODO: RootNodeOffset is always 0x20?
+            uint rootNodeOffset = 0x20;
+            writer.WriteUInt32(rootNodeOffset);
 
+            // TODO: ProjectNameOffset is always 0x38?
+            uint projectNameOffset = 0x38;
+            writer.WriteUInt32(projectNameOffset);
+            writer.WriteStringOffset(projectNameOffset, ProjectName);
+
+            // Align to 4 bytes if the last texture name wasn't
+            writer.Seek(0, SeekOrigin.End);
+            writer.Align(4);
+
+            // FontListOffset points to right after ProjectName
+            uint fontListOffset = (uint)(writer.Length - writer.GetOffsetOrigin());
+            writer.Seek(writer.GetOffsetOrigin() + 0x18, SeekOrigin.Begin);
             writer.WriteUInt32(DXLSignature);
-            writer.WriteUInt32(FontListOffset);
+            writer.WriteUInt32(fontListOffset);
 
-            writer.Seek(writer.GetOffsetOrigin() + RootNodeOffset, SeekOrigin.Begin);
+            writer.Seek(writer.GetOffsetOrigin() + rootNodeOffset, SeekOrigin.Begin);
             Root.Write(writer); // TODO: finish
 
-            writer.Seek(writer.GetOffsetOrigin() + FontListOffset, SeekOrigin.Begin);
+            writer.Seek(writer.GetOffsetOrigin() + fontListOffset, SeekOrigin.Begin);
             Fonts.Write(writer);
+
+            // Go back and write size
+            writer.Endianness = Endianness.Little;
+            {
+                writer.Seek(writer.GetOffsetOrigin() + 4, SeekOrigin.Begin);
+                writer.WriteUInt32((uint)writer.Length - dataStart);
+                writer.Seek(0, SeekOrigin.End);
+            }
+            writer.Endianness = endianPrev;
 
             writer.PopOffsetOrigin();
         }
