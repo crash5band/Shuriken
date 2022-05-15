c04813c7a1f944b3e22b8e49bfcfa2f16b81b584
Crash5b <crash5band@gmail.com>
2022-05-15 23:18:22 +0200

    Hide properties with respect to cast type

---
+++ Shuriken/Converters/EqualityToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Shuriken.Converters
+{
+    public class EqualityToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility v = Visibility.Visible;
+            if (value.ToString() != parameter.ToString())
+                v = Visibility.Collapsed;
+
+            return v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
