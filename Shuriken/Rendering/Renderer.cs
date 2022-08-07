8fad74111b65ef88c51f5c73871c4f96b65c5156
Crash5b <crash5band@gmail.com>
2022-08-07 20:23:44 +0200

    Refactor Renderer

--- Shuriken/Rendering/Renderer.cs
+++ Shuriken/Rendering/Renderer.cs
@@ -18,13 +18,9 @@ namespace Shuriken.Rendering
     {
         public readonly string shadersDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Shaders");
 
-        private uint vao;
-        private uint vbo;
-        private uint ebo;
-        private uint[] indices;
         public Dictionary<string, ShaderProgram> shaderDictionary;
 
-        private Vertex[] buffer;
+        private VertexBuffer vertexBuffer;
         private List<Quad> quads;
 
         private bool additive;
@@ -32,14 +28,7 @@ namespace Shuriken.Rendering
         private int textureId = -1;
         private ShaderProgram shader;
 
-        public readonly int MaxVertices = 10000;
-        public int MaxQuads => MaxVertices / 4;
-        public int MaxIndices => MaxQuads * 6;
-
-        public int NumVertices { get; private set; }
         public int NumQuads => quads.Count;
-        public int NumIndices { get; private set; }
-        public int BufferPos { get; private set; }
         public bool BatchStarted { get; private set; }
         public int Width { get; set; }
         public int Height { get; set; }
@@ -87,70 +76,16 @@ namespace Shuriken.Rendering
             ShaderProgram basicShader = new ShaderProgram("basic", Path.Combine(shadersDir, "basic.vert"), Path.Combine(shadersDir, "basic.frag"));
             shaderDictionary.Add(basicShader.Name, basicShader);
 
-            // setup vertex indices
-            indices = new uint[MaxIndices];
-            uint offset = 0;
-            for (uint index = 0; index < MaxIndices; index += 6)
-            {
-                indices[index + 0] = offset + 0;
-                indices[index + 1] = offset + 1;
-                indices[index + 2] = offset + 2;
-
-                indices[index + 3] = offset + 1;
-                indices[index + 4] = offset + 2;
-                indices[index + 5] = offset + 3;
-                
-                offset += 4;
-            }
-
-            buffer = new Vertex[MaxVertices];
-            quads = new List<Quad>(MaxQuads);
-            Init();
+            vertexBuffer = new VertexBuffer();
+            quads = new List<Quad>(vertexBuffer.MaxQuads);
 
             Width = width;
             Height = height;
         }
-        
-        private void Init()
+
+        ~Renderer()
         {
-            // 2 floats for pos, 2 floats for UVs, 4 floats for color
-            int stride = Unsafe.SizeOf<Vertex>();
-
-            GL.GenVertexArrays(1, out vao);
-            GL.BindVertexArray(vao);
-
-            GL.GenBuffers(1, out vbo);
-            GL.GenBuffers(1, out ebo);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-            GL.BufferData(BufferTarget.ArrayBuffer, MaxVertices, IntPtr.Zero, BufferUsageHint.DynamicDraw);
-
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, MaxIndices, indices, BufferUsageHint.StaticDraw);
-
-            // position
-            GL.EnableVertexAttribArray(0);
-            GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, stride, 0);
-
-            // uv
-            GL.EnableVertexAttribArray(1);
-            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, stride, 2 * sizeof(float));
-            
-            // color
-            GL.EnableVertexAttribArray(2);
-            GL.VertexAttribPointer(2, 4, VertexAttribPointerType.Float, false, stride, 4 * sizeof(float));
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            GL.BindVertexArray(0);
-        }
-
-        /// <summary>
-        /// Resets the number of quads, vertices, and indices.
-        /// </summary>
-        private void ResetRenderStats()
-        {
-            NumIndices = 0;
-            NumVertices = 0;
+            vertexBuffer.Dispose();
         }
 
         /// <summary>
@@ -158,10 +93,8 @@ namespace Shuriken.Rendering
         /// </summary>
         public void BeginBatch()
         {
-            BufferPos = 0;
+            vertexBuffer.ResetBufferPos();
             BatchStarted = true;
-
-            ResetRenderStats();
         }
 
         /// <summary>
@@ -169,34 +102,13 @@ namespace Shuriken.Rendering
         /// </summary>
         public void EndBatch()
         {
-            if (BufferPos > 0)
+            if (vertexBuffer.BufferPos > 0)
             {
-                GL.BindVertexArray(vao);
-                GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-                GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
-                GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, BufferPos * Unsafe.SizeOf<Vertex>(), buffer);
-                Flush();
+                vertexBuffer.Upload();
+                vertexBuffer.Flush();
             }
 
             BatchStarted = false;
-        }
-
-        private void Flush()
-        {
-            GL.DrawElements(PrimitiveType.Triangles, NumIndices, DrawElementsType.UnsignedInt, 0);
-        }
-        
-        /// <summary>
-        /// Pushes the quad parameters onto the vertex buffer.
-        /// </summary>
-        /// <param name="q">The quad to push to the buffer.</param>
-        public void PushQuad(Quad q)
-        {
-            buffer[BufferPos++] = q.TopLeft;
-            buffer[BufferPos++] = q.BottomLeft;
-            buffer[BufferPos++] = q.TopRight;
-            buffer[BufferPos++] = q.BottomRight;
-            NumIndices += 6;
         }
         
         public void DrawSprite(
@@ -284,7 +196,8 @@ namespace Shuriken.Rendering
             {
                 int id = quad.Texture?.GlTex?.ID ?? -1;
 
-                if (id != TextureId || Additive != quad.Additive || LinearFiltering != quad.LinearFiltering || NumVertices >= MaxVertices)
+                if (id != TextureId || Additive != quad.Additive || LinearFiltering != quad.LinearFiltering ||
+                    vertexBuffer.NumVertices >= vertexBuffer.MaxVertices)
                 {
                     EndBatch();
                     BeginBatch();
@@ -296,7 +209,7 @@ namespace Shuriken.Rendering
                     LinearFiltering = quad.LinearFiltering;
                 }
 
-                PushQuad(quad);
+                vertexBuffer.PushQuad(quad);
             }
 
             if (BatchStarted)
---
+++ Shuriken/Rendering/VertexBuffer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using System.Runtime.CompilerServices;
+
+namespace Shuriken.Rendering
+{
+    public class VertexBuffer
+    {
+        private uint vao;
+        private uint vbo;
+        private uint ebo;
+        private uint[] indices;
+
+        private Vertex[] buffer;
+
+        public readonly int MaxVertices = 10000;
+        public int MaxQuads => MaxVertices / 4;
+        public int MaxIndices => MaxQuads * 6;
+
+        public int NumVertices { get; private set; }
+        public int NumIndices { get; private set; }
+        public int BufferPos { get; private set; }
+
+        private void AllocateBuffer()
+        {
+            indices = new uint[MaxIndices];
+            uint offset = 0;
+            for (uint index = 0; index < MaxIndices; index += 6)
+            {
+                indices[index + 0] = offset + 0;
+                indices[index + 1] = offset + 1;
+                indices[index + 2] = offset + 2;
+
+                indices[index + 3] = offset + 1;
+                indices[index + 4] = offset + 2;
+                indices[index + 5] = offset + 3;
+
+                offset += 4;
+            }
+
+            buffer = new Vertex[MaxVertices];
+        }
+
+        public void Initialize()
+        {
+            AllocateBuffer();
+
+            // 2 floats for pos, 2 floats for UVs, 4 floats for color
+            int stride = Unsafe.SizeOf<Vertex>();
+
+            GL.GenVertexArrays(1, out vao);
+            GL.BindVertexArray(vao);
+
+            GL.GenBuffers(1, out vbo);
+            GL.GenBuffers(1, out ebo);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, MaxVertices, IntPtr.Zero, BufferUsageHint.DynamicDraw);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, MaxIndices, indices, BufferUsageHint.StaticDraw);
+
+            // position
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, stride, 0);
+
+            // uv
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, stride, 2 * sizeof(float));
+
+            // color
+            GL.EnableVertexAttribArray(2);
+            GL.VertexAttribPointer(2, 4, VertexAttribPointerType.Float, false, stride, 4 * sizeof(float));
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+        }
+
+        /// <summary>
+        /// Resets the vertex buffer's pointer
+        /// </summary>
+        public void ResetBufferPos()
+        {
+            BufferPos = 0;
+        }
+
+        /// <summary>
+        /// Uploads the vertex buffer's contents to the GPU
+        /// </summary>
+        public void Upload()
+        {
+            GL.BindVertexArray(vao);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, BufferPos * Unsafe.SizeOf<Vertex>(), buffer);
+        }
+
+        /// <summary>
+        /// Pushes the quad parameters onto the vertex buffer.
+        /// </summary>
+        /// <param name="q">The quad to push to the buffer.</param>
+        public void PushQuad(Quad q)
+        {
+            buffer[BufferPos++] = q.TopLeft;
+            buffer[BufferPos++] = q.BottomLeft;
+            buffer[BufferPos++] = q.TopRight;
+            buffer[BufferPos++] = q.BottomRight;
+            NumVertices += 4;
+            NumIndices += 6;
+        }
+
+        /// <summary>
+        /// Draws the vertex buffer's contents
+        /// </summary>
+        public void Flush()
+        {
+            GL.DrawElements(PrimitiveType.Triangles, NumIndices, DrawElementsType.UnsignedInt, 0);
+            NumVertices = 0;
+            NumIndices = 0;
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteVertexArray(vao);
+            GL.DeleteBuffer(vbo);
+            GL.DeleteBuffer(ebo);
+        }
+
+        public VertexBuffer()
+        {
+            vao = vbo = ebo = 0;
+            BufferPos = 0;
+
+            Initialize();
+        }
+    }
+}
